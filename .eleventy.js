33f8a3a48b81f5efd40a9153347419d1d804d5e4
Spiritusanti <mccrackenjacob@gmail.com>
2023-01-17 09:08:12 -0800

    updating main

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles");
   eleventyConfig.addPassthroughCopy("./src/assests");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/_posts");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: { input: "src", output: "_site" },
